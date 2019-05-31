f0e64ae5cf250bca530e283c221cb03679c94f21
System Administrator <none>
2019-05-31 19:31:59 +0200

    Changes by 'System Administrator' on 'CICD Test Application – 1.0.0' -- Build #1

---
+++ sn/CICD Test Application/Script Include/active_true/x_11413_cicd_test.testMe.js
@@ -0,0 +1,37 @@
+/* 
+ * Application : CICD Test Application
+ * ClassName   : sys_script_include
+ * Created On  : 2019-05-31 17:23:14
+ * Created By  : admin
+ * Updated On  : 2019-05-31 17:25:31
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=22b2c889dbb133001ac4a565ca9619d1
+ */
+/**
+ * Class Description
+ * 
+ * @class 
+ * @author System Administrator [admin]
+ * @memberof x_11413_cicd_test.module:sys_script_include
+ */
+var testMe = Class.create();
+testMe.prototype = /** @lends x_11413_cicd_test.module:sys_script_include.testMe.prototype */ {
+    /**
+     * Description
+     * 
+     * @returns {undefined}
+     */
+    initialize: function() {
+    },
+	
+	/**
+	 * Description
+	 * 
+	 * @returns {boolean}
+	 */
+	openMe: function() {
+		return false==false;
+	},
+
+    type: 'testMe'
+};
